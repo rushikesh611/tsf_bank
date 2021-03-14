7d3f2e3f2ea8e2e376489d8b0a952e4a424a0ba2
hrushikesh jadhav <rushi611@gmail.com>
2021-03-14 13:47:14 +0530

    deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn tsfbank.wsgi 
--- tsfbank/settings.py
+++ tsfbank/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
